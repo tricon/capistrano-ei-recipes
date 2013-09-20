b9acee8440ab9e6ef2a188b554874e437fc88225
David Aaron Fendley <tricon@me.com>
2013-09-20 09:49:01 -0500

    Don't use shared dir.

--- lib/capistrano-ei-recipes/bundler.rb
+++ lib/capistrano-ei-recipes/bundler.rb
@@ -1,15 +1,3 @@
 unless Capistrano::Configuration.respond_to?(:instance)
   abort 'capistrano/ext/multistage requires Capistrano 2'
 end
-
-Capistrano::Configuration.instance.load do
-  namespace :bundler do
-    task :create_symlink, :roles => :app do
-      shared_dir = File.join(shared_path, 'bundle')
-      release_dir = File.join(current_release, 'vendor')
-      run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-    end
-  end
-
-  after 'deploy:setup', 'bundler:create_symlink'
-end
